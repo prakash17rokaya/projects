7aff46da268340b4cf674798e57ef8cb9e565f90
prakash17rokaya <119596080+prakash17rokaya@users.noreply.github.com>
2022-12-01 17:52:56 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './Style.scss';
+import { Routes, Route, Link } from "react-router-dom";
+import Header from './Comp/Header';
+import Footer from './Comp/Footer';
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    
+    
+
+    </div>
+  );
+}
+
+export default App;
